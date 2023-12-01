89ab043b477e87038f903f4a5d474893fd12f4d3
João Paulo Gomes <johnowl@gmail.com>
2023-12-01 23:40:06 +0100

    Day 1

---
+++ src/main/kotlin/day1/Part1.kt
@@ -0,0 +1,22 @@
+package day1
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val result = File("src/main/resources/input.txt")
+        .readLines()
+        .sumOf {
+            "${it.firstDigit()}${it.lastDigit()}".toInt()
+        }
+
+    println(result)
+}
+
+private fun String.firstDigit(): Char = this.find {
+    it.isDigit()
+} ?: error("No digit found in string")
+
+
+private fun String.lastDigit(): Char = this.findLast {
+    it.isDigit()
+} ?: error("No digit found in string")
---
+++ src/main/kotlin/day1/Part2.kt
@@ -0,0 +1,56 @@
+package day1
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val result = File("src/main/resources/input.txt")
+        .readLines()
+        .sumOf {
+            "${it.firstDigit()}${it.lastDigit()}".toInt()
+        }
+
+    println(result)
+
+    // 53846 = wrong
+    // 53855
+}
+
+
+private val digits = listOf(
+    "zero",
+    "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine"
+)
+
+private fun String.firstDigit(): String {
+    for (i in this.indices) {
+        if (this[i].isDigit()) {
+            return this[i].toString()
+        }
+
+       for (digit in digits) {
+           if (this.substring(i).startsWith(digit)) {
+               return (digits.indexOf(digit)).toString()
+           }
+       }
+    }
+
+    error("No digit found in string $this")
+}
+
+
+private fun String.lastDigit(): String {
+    for (i in this.length - 1 downTo 0) {
+        if (this[i].isDigit()) {
+            return this[i].toString()
+        }
+
+        for (digit in digits) {
+            if (this.substring(0, i + 1).endsWith(digit)) {
+                return (digits.indexOf(digit)).toString()
+            }
+        }
+    }
+
+    error("No digit found in string $this")
+}
