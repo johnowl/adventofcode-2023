79e0ff540aa0cefab40a23a5555dc306a62ba075
João Paulo Gomes <johnowl@gmail.com>
2023-12-07 23:51:10 +0100

    Day 7 (WIP)

---
+++ src/main/kotlin/day7/Day7Part1.kt
@@ -0,0 +1,207 @@
+package day7
+
+import java.io.File
+
+val day7Part1TestInput = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+""".trimIndent()
+
+data class Card(val label: String) {
+    private val cardsOrder = listOf("A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J").reversed()
+
+    fun strength(): Int {
+        val cardIndex = cardsOrder.indexOf(label)
+        return cardIndex + 1
+    }
+}
+
+enum class HandType(val power: Int) {
+    FiveOfAKind(7), // AAAAA
+    FourOfAKind(6), // AAAAB
+    FullHouse(5),   // AAABB
+    ThreeOfAKind(4),// AAABC
+    TwoPair(3),     // AABBC
+    OnePair(2),     // AABCD
+    HighCard(1);    // ABCDE
+
+    fun increment(sortedCards: List<List<Card>>): HandType {
+        val cards = sortedCards.flatten()
+
+        val totalJokers = cards.countJokers()
+        if (totalJokers == 0) {
+            return this
+        }
+
+        val amountOfJokersInHandType = when (this) {
+            FiveOfAKind, HighCard -> cards.countJokers()
+            FourOfAKind, ThreeOfAKind -> sortedCards[0].countJokers()
+            TwoPair, FullHouse -> sortedCards[0].countJokers() + sortedCards[1].countJokers()
+            OnePair -> sortedCards[0].countJokers()
+        }
+        val unusedJokers = totalJokers - amountOfJokersInHandType
+
+        return when (this) {
+            FiveOfAKind ->
+                // AAAAA
+                // JJJJJ
+                FiveOfAKind
+
+            FourOfAKind -> {
+                // AAAAJ
+                // AAAAB
+                // JJJJA
+                when (unusedJokers) {
+                    0 -> FourOfAKind
+                    1 -> FiveOfAKind
+                    else -> error("Invalid amount of jokers")
+                }
+            }
+
+            FullHouse -> {
+                // AAABB
+                // AAAJJ
+                // JJJAA
+                if (amountOfJokersInHandType == 2) FiveOfAKind else FullHouse
+            }
+
+            ThreeOfAKind -> {
+                // AAABJ
+                // AAABC
+                // JJJAB -> ABBAB
+                when (unusedJokers) {
+                    0 -> if (amountOfJokersInHandType == 3) FullHouse else ThreeOfAKind
+                    1 -> FourOfAKind
+                    else -> error("Invalid amount of jokers")
+                }
+            }
+
+            TwoPair -> {
+                // AABBJ
+                // AAJJB
+                // AABBC
+                when (unusedJokers) {
+                    0 -> if (amountOfJokersInHandType == 2) FourOfAKind else TwoPair
+                    1 -> FullHouse
+                    else -> error("Invalid amount of jokers")
+                }
+            }
+
+            OnePair -> {
+                // AABCJ
+                // AABCD
+                // JJABC
+                when (unusedJokers) {
+                    0 -> if (amountOfJokersInHandType == 2) ThreeOfAKind else OnePair
+                    1 -> TwoPair
+                    else -> error("Invalid amount of jokers")
+                }
+            }
+
+            HighCard -> {
+                // ABCDE
+                // JABCD
+                if (amountOfJokersInHandType == 1) OnePair else HighCard
+            }
+        }
+    }
+
+    private fun List<Card>.countJokers(): Int =
+        this.count { it.label == "J" }
+}
+
+data class Hand(val cards: List<Card>, val bid: Long) {
+
+    fun calculateHandType(): HandType {
+        val groupedCards = cards.groupBy { it.label }
+        val sortedCards = groupedCards.values.sortedByDescending { it.size }
+        val handType = when (sortedCards.size) {
+            1 -> HandType.FiveOfAKind
+            2 -> {
+                if (sortedCards[0].size == 4) {
+                    HandType.FourOfAKind
+                } else {
+                    HandType.FullHouse
+                }
+            }
+
+            3 -> {
+                if (sortedCards[0].size == 3) {
+                    HandType.ThreeOfAKind
+                } else {
+                    HandType.TwoPair
+                }
+            }
+
+            4 -> HandType.OnePair
+            else -> HandType.HighCard
+        }
+
+        return handType.increment(sortedCards)
+    }
+
+    fun print() {
+        print("${this.cards.joinToString(separator = "") { it.label }},${this.calculateHandType()}")
+    }
+
+    fun calculateValue(): Long =
+        cards.joinToString("") {
+            "$0${it.strength()}".takeLast(2)
+        }.toLong()
+
+}
+
+data class Game(val hands: List<Hand>) {
+    private fun rankHands(): List<Hand> =
+        hands.sortedBy { it.calculateHandType().power }
+            .groupBy { it.calculateHandType() }
+            .flatMap {
+                it.value.sortedBy { hand ->
+                    hand.calculateValue()
+                }
+            }
+
+    fun calculateBids(): List<Long> =
+        buildList {
+            for ((index, hand) in rankHands().withIndex()) {
+                val position = index + 1
+                print("$position,")
+                hand.print()
+                println()
+                val bid = hand.bid * position
+                this.add(bid)
+            }
+        }
+
+}
+
+fun test() {
+    val game = parse(day7Part1TestInput)
+    game.calculateBids().sum().let(::println)
+}
+
+fun main() {
+    test()
+    File("src/main/resources/day7/input.txt")
+        .readText()
+        .let { input ->
+            val game = parse(input)
+            game.calculateBids().sum().let(::println)
+        }
+    // 253919701 22:30
+}
+
+fun parse(input: String): Game =
+    input.split("\n").map { line ->
+        val (cards, bid) = line.split(" ").filter { it.isNotBlank() }
+        val parsedCards = cards.split("").filter { it.isNotBlank() }.map(::Card)
+        Hand(parsedCards, bid.toLong())
+    }.let {
+        Game(it)
+    }
+
+
+
--- src/test/kotlin/day6/RaceTest.kt
+++ src/test/kotlin/day6/RaceTest.kt
@@ -1,7 +1,6 @@
 package day6
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class RaceTest {
